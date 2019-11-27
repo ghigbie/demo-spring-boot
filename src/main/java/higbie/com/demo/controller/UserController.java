f28550db4125c9b92f035a548f89c5a96b7050d3
ghigbie <gehigbie@gmail.com>
2019-11-26 20:43:25 -0700

    Add methods to user Controller

--- src/main/java/higbie/com/demo/controller/UserController.java
+++ src/main/java/higbie/com/demo/controller/UserController.java
@@ -1,4 +1,23 @@
 package higbie.com.demo.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @RequestMapping("/{userId}")
+    public String displayUser(@PathVariable int userId){
+        return"User Found! It's User: "+ userId;
+    }
+
+    @RequestMapping("/{userId}/invoices")
+    public String displayInvoices(@PathVariable int userId, @RequestParam Date date){
+        return"Invoice found for user: " + userId + " on the date: " + date;
+    }
 }
