fead2def75ffc441dbbe9b1843cb7e72d260d515
ghigbie <gehigbie@gmail.com>
2019-11-27 20:25:18 -0700

    Create user class

--- src/main/java/higbie/com/demo/controller/HomeController.java
+++ src/main/java/higbie/com/demo/controller/HomeController.java
@@ -2,6 +2,7 @@ package higbie.com.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,10 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         return "index";
+    }
+
+    @RequestMapping(value="/create", method= RequestMethod.POST)
+    public String processFormData(){
+        return "Form done!";
     }
 }
