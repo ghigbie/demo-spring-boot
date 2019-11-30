828b228fbba8a11c26d2b1f66b01157c89c4e0ee
ghigbie <gehigbie@gmail.com>
2019-11-30 11:03:24 -0700

    Add redirect to home controller

--- src/main/java/higbie/com/demo/controller/HomeController.java
+++ src/main/java/higbie/com/demo/controller/HomeController.java
@@ -5,18 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value="/", method=RequestMethod.GET)
+    @RequestMapping("/")
     public String home(Model model){
         model.addAttribute("formData", new User());
         return "index";
     }
 
-    @RequestMapping(value="/create", method=RequestMethod.POST)
-    public String processFormData(User user){
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String processFormData(User user, RedirectAttributes attr){
+        attr.addFlashAttribute("user", user);
+        return "redirect:/display";
+    }
+
+    @RequestMapping(value = "/display", method = RequestMethod.POST)
+    public String displayFormData(User user ){
         return "result";
     }
 }
