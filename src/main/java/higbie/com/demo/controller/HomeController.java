63d022a6fdce8ebe3d14980dbe8416f2f2479e24
ghigbie <gehigbie@gmail.com>
2019-11-30 10:24:06 -0700

    Edit templates

--- src/main/java/higbie/com/demo/controller/HomeController.java
+++ src/main/java/higbie/com/demo/controller/HomeController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping(value="/", method=RequestMethod.GET)
     public String home(Model model){
         model.addAttribute("formData", new User());
-        return "index.html";
+        return "index";
     }
 
     @RequestMapping(value="/create", method=RequestMethod.POST)
-    public String processFormData(){
-        return "Form done!";
+    public String processFormData(User user){
+        return "result";
     }
 }
